cfb37d9608f42a34ed3b8bfa5996e4a7f8ae9440
Razee_jafri <jafrirazee@gmail.com>
2026-04-06 12:58:13 +0530

    re

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -25,7 +25,7 @@ import { useEffect } from 'react'
 import { io } from 'socket.io-client'
 import { setSocket } from './redux/userSlice'
 
-export const serverUrl = import.meta.env.VITE_SERVER_URL
+export const serverUrl = "https://vingo-backend-up94.onrender.com"
 function App() {
   const { userData } = useSelector(state => state.user)
   const dispatch = useDispatch()
